81471bd9841c8a1be6bfeec45a2c4c2e4275f7cc
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-18 02:20:43 +0530

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -16,7 +16,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 POTATO_ROOT="${MY_DIR}"/../../..
 
-HELPER="${POTATO_ROOT}/vendor/potato/build/tools/extract_utils.sh"
+HELPER="${POTATO_ROOT}/vendor/cesium/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
