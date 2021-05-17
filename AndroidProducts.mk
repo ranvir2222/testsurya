6791416e437e870c6477fb5d18c2f26a028abd41
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-18 02:22:33 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/potato_surya.mk
+    $(LOCAL_DIR)/cesium_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    potato_surya-user \
-    potato_surya-userdebug \
-    potato_surya-eng
+    cesium_surya-user \
+    cesium_surya-userdebug \
+    cesium_surya-eng
