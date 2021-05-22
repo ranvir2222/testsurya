5715113ff8df89a1412e2ba69b915f73d11bdfb9
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-22 16:38:27 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cesium_surya.mk
+    $(LOCAL_DIR)/awaken_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    cesium_surya-user \
-    cesium_surya-userdebug \
-    cesium_surya-eng
+    awaken_surya-user \
+    awaken_surya-userdebug \
+    awaken_surya-eng
